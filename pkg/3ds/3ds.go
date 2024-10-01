a2e6c6b53dd9452d7f7d941709ceee38e311341d
Victor Aguilera <victor.aguilera_salleras@mail.schwarz>
2024-10-01 17:29:19 +0200

    Read materials and all the meshes

--- pkg/3ds/3ds.go
+++ pkg/3ds/3ds.go
@@ -12,17 +12,42 @@ import (
 const (
 	MAIN_CHUNK       = 0x4D4D
 	EDIT3DS_CHUNK    = 0x3D3D
+	EDIT_MATERIAL    = 0xAFFF
+	MAT_NAME01       = 0xA000
 	KEYF3DS_CHUNK    = 0xB000
 	EDITOBJECT_CHUNK = 0x4000
 	OBJTRIMESH_CHUNK = 0x4100
 	TRIVERTEXL_CHUNK = 0x4110
 	TRILOCAL_CHUNK   = 0x4160
 	TRIFACEL_CHUNK   = 0x4120
+	TRI_MAPPINGCOORS = 0x4140
+
+	COLOR24           = 0x0011
+	MATERIAL_AMBIENT  = 0xa010
+	MATERIAL_DIFFUSE  = 0xa020
+	MATERIAL_SPECULAR = 0xa030
+	MATERIAL_TEXMAP   = 0xa200
+	MATERIAL_MAPNAME  = 0xa300
 )
 
 type F3DS struct {
-	vertexList []Vertex
-	facesList  []Face
+	Meshes    []Mesh
+	Materials []Material
+}
+
+type Mesh struct {
+	Name       string
+	VertexList []Vertex
+	FacesList  []Face
+	CoordsList []Coord
+}
+
+type Material struct {
+	Name     string
+	File     string
+	Ambient  Color24
+	Diffuse  Color24
+	Specular Color24
 }
 
 type Chunk struct {
@@ -37,6 +62,14 @@ type Vertex struct {
 
 type Face struct {
 	V1, V2, V3, Info uint16
+}
+
+type Coord struct {
+	U, V float32
+}
+
+type Color24 struct {
+	R, G, B byte
 }
 
 func readChunk(file io.Reader) (*Chunk, error) {
@@ -62,65 +95,176 @@ func readChunk(file io.Reader) (*Chunk, error) {
 	return &Chunk{ID: id, Len: length, Data: data}, nil
 }
 
-func (f3ds *F3DS) processFaces(data []byte) error {
-	r := bytes.NewReader(data)
-	var numFaces uint16
-
-	if err := binary.Read(r, binary.LittleEndian, &numFaces); err != nil {
-		return err
-	}
-	fmt.Printf("Num of Faces: %d\n", numFaces)
-
-	for i := uint16(0); i < numFaces; i++ {
-		var face Face
-		if err := binary.Read(r, binary.LittleEndian, &face); err != nil {
-			return fmt.Errorf("error reading faces: %w", err)
+func find0Byte(data []byte) int {
+	idx := 0
+	var v byte
+	for idx, v = range data {
+		if v == 0 {
+			break
 		}
-		f3ds.facesList = append(f3ds.facesList, face)
 	}
-	return nil
+	return idx
 }
 
-func (f3ds *F3DS) processTriangleList(data []byte) error {
-	r := bytes.NewReader(data)
-	var numVert uint16
-	if err := binary.Read(r, binary.LittleEndian, &numVert); err != nil {
-		return err
-	}
-	fmt.Printf("Num of vertex: %d\n", numVert)
-
-	for i := uint16(0); i < numVert; i++ {
-		var vert Vertex
-		if err := binary.Read(r, binary.LittleEndian, &vert); err != nil {
-			return fmt.Errorf("error reading vertices: %w", err)
-		}
-		f3ds.vertexList = append(f3ds.vertexList, vert)
-	}
-	return nil
-}
-
-func (f3ds *F3DS) processObjTrimesh(data []byte) error {
+func processMaterialTexMap(data []byte) (string, error) {
 	r := bytes.NewReader(data)
 	for {
 		chunk, err := readChunk(r)
 		if err != nil {
 			if err == io.EOF {
-				if len(f3ds.vertexList) == 0 {
-					return errors.New("error. No triangle data found in mesh")
-				} else {
-					return nil
-				}
+				return "", errors.New("no material filename found")
+			}
+			return "", fmt.Errorf("error reading file: %s", err)
+		}
+		switch chunk.ID {
+		case MATERIAL_MAPNAME:
+			idx := find0Byte(chunk.Data)
+			return string(chunk.Data[:idx]), nil
+		default:
+			// fmt.Printf("Ignored chuck: %x\n", chunk.ID)
+		}
+	}
+}
+
+func readColor24(data []byte) (Color24, error) {
+	r := bytes.NewReader(data)
+	chunk, _ := readChunk(r)
+	if chunk.ID != COLOR24 {
+		return Color24{}, fmt.Errorf("expecting Color24 chunk. Found: %d", chunk.ID)
+	}
+	r = bytes.NewReader(chunk.Data)
+	var color Color24
+	if err := binary.Read(r, binary.LittleEndian, &color); err != nil {
+		return Color24{}, fmt.Errorf("error reading Color24: %w", err)
+	}
+	return color, nil
+}
+
+func (f3ds *F3DS) processMaterial(data []byte) (Material, error) {
+	r := bytes.NewReader(data)
+	var material Material
+	for {
+		chunk, err := readChunk(r)
+		if err != nil {
+			if err == io.EOF {
+				return material, nil
+			}
+			return material, fmt.Errorf("error reading file: %s", err)
+		}
+		switch chunk.ID {
+		case MAT_NAME01:
+			idx := find0Byte(chunk.Data)
+			material.Name = string(chunk.Data[:idx])
+		case MATERIAL_AMBIENT:
+			if color, err := readColor24(chunk.Data); err != nil {
+				return material, err
+			} else {
+				material.Ambient = color
+			}
+		case MATERIAL_DIFFUSE:
+			if color, err := readColor24(chunk.Data); err != nil {
+				return material, err
+			} else {
+				material.Diffuse = color
+			}
+		case MATERIAL_SPECULAR:
+			if color, err := readColor24(chunk.Data); err != nil {
+				return material, err
+			} else {
+				material.Specular = color
+			}
+		case MATERIAL_TEXMAP:
+			if name, err := processMaterialTexMap(chunk.Data); err != nil {
+				return material, err
+			} else {
+				material.File = name
+			}
+		default:
+			// fmt.Printf("ffffIgnored chuck: %x - %s\n", chunk.ID, chunk.Data)
+		}
+	}
+
+	//return nil
+}
+
+func (f3ds *F3DS) processCoordinates(data []byte) ([]Coord, error) {
+	r := bytes.NewReader(data)
+	var numVertex uint16
+
+	if err := binary.Read(r, binary.LittleEndian, &numVertex); err != nil {
+		return nil, err
+	}
+
+	var coordList []Coord
+	for i := uint16(0); i < numVertex; i++ {
+		var coord Coord
+		if err := binary.Read(r, binary.LittleEndian, &coord); err != nil {
+			return nil, fmt.Errorf("error reading texture coordinates: %w", err)
+		}
+		coordList = append(coordList, coord)
+	}
+	return coordList, nil
+}
+
+func (f3ds *F3DS) processFaces(data []byte) ([]Face, error) {
+	r := bytes.NewReader(data)
+	var numFaces uint16
+
+	if err := binary.Read(r, binary.LittleEndian, &numFaces); err != nil {
+		return nil, err
+	}
+
+	var faceList []Face
+	for i := uint16(0); i < numFaces; i++ {
+		var face Face
+		if err := binary.Read(r, binary.LittleEndian, &face); err != nil {
+			return nil, fmt.Errorf("error reading faces: %w", err)
+		}
+		faceList = append(faceList, face)
+	}
+	return faceList, nil
+}
+
+func (f3ds *F3DS) processTriangleList(data []byte) ([]Vertex, error) {
+	r := bytes.NewReader(data)
+	var numVert uint16
+	if err := binary.Read(r, binary.LittleEndian, &numVert); err != nil {
+		return nil, err
+	}
+
+	var vertexList []Vertex
+	for i := uint16(0); i < numVert; i++ {
+		var vert Vertex
+		if err := binary.Read(r, binary.LittleEndian, &vert); err != nil {
+			return nil, fmt.Errorf("error reading vertices: %w", err)
+		}
+		vertexList = append(vertexList, vert)
+	}
+	return vertexList, nil
+}
+
+func (f3ds *F3DS) processObjTrimesh(data []byte, mesh *Mesh) error {
+	r := bytes.NewReader(data)
+	for {
+		chunk, err := readChunk(r)
+		if err != nil {
+			if err == io.EOF {
+				return nil
 			}
 			return fmt.Errorf("error reading file: %s", err)
 		}
 
 		switch chunk.ID {
 		case TRIVERTEXL_CHUNK:
-			if err = f3ds.processTriangleList(chunk.Data); err != nil {
+			if mesh.VertexList, err = f3ds.processTriangleList(chunk.Data); err != nil {
 				return err
 			}
 		case TRIFACEL_CHUNK:
-			if err = f3ds.processFaces(chunk.Data); err != nil {
+			if mesh.FacesList, err = f3ds.processFaces(chunk.Data); err != nil {
+				return err
+			}
+		case TRI_MAPPINGCOORS:
+			if mesh.CoordsList, err = f3ds.processCoordinates(chunk.Data); err != nil {
 				return err
 			}
 		default:
@@ -130,26 +274,24 @@ func (f3ds *F3DS) processObjTrimesh(data []byte) error {
 }
 
 func (f3ds *F3DS) processEditObject(data []byte) error {
-	idx := 0
-	var v byte
-	for idx, v = range data {
-		if v == 0 {
-			break
-		}
-	}
+	idx := find0Byte(data)
 	name := string(data[:idx])
-	fmt.Printf("object name: %s\n", name)
+	mesh := Mesh{Name: name}
+	//fmt.Printf("object name: %s\n", name)
 	r := bytes.NewReader(data[idx+1:])
 	for {
 		chunk, err := readChunk(r)
 		if err != nil {
 			if err == io.EOF {
-				return errors.New("error. No triangle data found in mesh")
+				return nil
 			}
 			return fmt.Errorf("error reading file: %s", err)
 		}
 		if chunk.ID == OBJTRIMESH_CHUNK {
-			return f3ds.processObjTrimesh(chunk.Data)
+			if err := f3ds.processObjTrimesh(chunk.Data, &mesh); err != nil {
+				return err
+			}
+			f3ds.Meshes = append(f3ds.Meshes, mesh)
 		}
 	}
 }
@@ -160,12 +302,23 @@ func (f3ds *F3DS) processEditorChunk(data []byte) error {
 		chunk, err := readChunk(r)
 		if err != nil {
 			if err == io.EOF {
-				return errors.New("cannot find any mesh in the file")
+				return nil
 			}
 			return fmt.Errorf("error reading file: %s", err)
 		}
-		if chunk.ID == EDITOBJECT_CHUNK {
-			return f3ds.processEditObject(chunk.Data)
+		switch chunk.ID {
+		case EDITOBJECT_CHUNK:
+			if err = f3ds.processEditObject(chunk.Data); err != nil {
+				return err
+			}
+		case EDIT_MATERIAL:
+			if material, err := f3ds.processMaterial(chunk.Data); err != nil {
+				return err
+			} else {
+				f3ds.Materials = append(f3ds.Materials, material)
+			}
+		default:
+			fmt.Printf("Ignored chuck: %x\n", chunk.ID)
 		}
 	}
 }
@@ -202,7 +355,11 @@ func (f3ds *F3DS) LoadFile(file string) error {
 	err = f3ds.processFile(f)
 	if err != nil {
 		fmt.Println(err)
+		fmt.Printf("%#v\n", f3ds.Meshes)
 	}
+	for _, m := range f3ds.Meshes {
+		fmt.Printf("%s\t\tv: %d\t\tf: %d\t\tcoord: %d\n", m.Name, len(m.VertexList), len(m.FacesList), len(m.CoordsList))
 
+	}
 	return nil
 }
